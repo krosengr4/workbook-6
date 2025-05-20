ff29bc58f9342f38ecf84c06300697d1927cf095
krosengr4 <rosenkev4@gmail.com>
2025-05-20 12:14:19 -0500

    Completed WorkingWithInterfaces exercise plus the bonus. Exercise 2, module 2, workbook 6

--- WorkingWithInterfaces/src/main/java/WorkingWithInterfaces/Person.java
+++ WorkingWithInterfaces/src/main/java/WorkingWithInterfaces/Person.java
@@ -1,2 +1,56 @@
-public class Person {
+package WorkingWithInterfaces;
+
+public class Person implements Comparable<Person>{
+
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    //region getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    //endregion
+
+    //The object inside the compareTo() is o
+    public int compareTo(Person o) {
+
+        if (this.lastName.compareTo(o.lastName) != 0) {
+            return this.lastName.compareTo(o.lastName);
+        } else if (this.firstName.compareTo(o.firstName) != 0) {
+            return this.firstName.compareTo(o.firstName);
+        } else if (this.age == o.age){
+            return 0;
+        } else if (this.age > o.age) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
--- WorkingWithInterfaces/src/test/java/WorkingWithInterfaces/PersonTest.java
+++ WorkingWithInterfaces/src/test/java/WorkingWithInterfaces/PersonTest.java
@@ -1,4 +1,51 @@
+package WorkingWithInterfaces;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonTest {
-  
+
+    @Test
+    void compareTo() {
+
+        Person me = new Person("Kevin", "Rosengren", 245);
+        Person you = new Person("Susie", "Snazzerton", 123);
+
+        int actual = me.compareTo(you);
+
+        //R minus S equals -1
+        assertEquals(-1, actual);
+    }
+
+    @Test
+    void compareSameLastName() {
+        Person p1 = new Person("Alex", "Last", 19);
+        Person p2 = new Person("Zach", "Last", 21);
+
+        int actual = p1.compareTo(p2);
+
+        //A minus Z equals -25
+        assertEquals(-25, actual);
+    }
+
+    @Test
+    void compareSameFirstAndLast() {
+        Person p1 = new Person("First", "Last", 30);
+        Person p2 = new Person("First", "Last", 20);
+
+        int actual = p1.compareTo(p2);
+
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void compareSameFirstLastAndAge() {
+        Person p1 = new Person("First", "Last", 30);
+        Person p2 = new Person("First", "Last", 30);
+
+        int actual = p1.compareTo(p2);
+
+        assertEquals(0, actual);
+    }
 }
